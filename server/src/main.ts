8523cb589d1bddc6fa0696f94d2e2b077d86cc97
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2023-10-06 21:44:03 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,9 +7,9 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('All good');
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
