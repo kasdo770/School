bca73ea0601685db2be0ac504acd27ce1543a564
Jeyenor <78587875+Jeyenore26@users.noreply.github.com>
2023-10-11 15:40:47 +0300

    3

--- client/app/register/page.jsx
+++ client/app/register/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Flex, Text, Button } from '@radix-ui/themes';
+import { Flex, Text, Heading } from '@radix-ui/themes';
 
 export default function Home() {
   return (
@@ -7,10 +7,16 @@ export default function Home() {
       <Image src={"/register.jpg"} width={5000}
         height={5000}
         className="w-full z-[-8] absolute object-cover h-screen" />
-      <container className="w-[619px] h-[727px] bg-white shadow-2xl rounded-2xl border-black border-[1px]">
-        <div className="flex items-end h-full" >
-          <Image src={"/bluewave.png"} width={5000} height={100} className="w-full rounded-2xl" />
+      <container className="w-[619px] h-fit bg-white shadow-2xl rounded-2xl border-black border-[1px] z-[-2]">
+        <div className="flex justify-center ">
+          <h1 className="mt-[20px] text-2xl font-black	">اسكولا</h1>
+
         </div>
+        <div className="flex items-end h-full  " >
+          <Image src={"/bluewave.png"} width={5000} height={100} className="w-fill h-auto rounded-2xl mt-[0px]" />
+        </div>
+
+
       </container>
     </div>
   )
