c5f31d0185eee0f542beade9d857dd3f251a808d
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2023-10-11 07:31:12 +0300

    database In

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["4kwallpapers.com"],
   },
+  env: {
+    mongodburl: "mongodb+srv://lordskasdo:Albosaty123@school.dqxwdmb.mongodb.net/"
+  }
 };
 
 module.exports = nextConfig;
