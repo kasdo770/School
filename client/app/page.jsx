bfd8e9da60dcecca1b745defc2fa193c6d681a39
Jeyenore <iiherogamer18@gmail.com>
2023-10-11 14:17:21 +0300

    ACTUALLY responsive

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -3,46 +3,47 @@ import { AiOutlineDownCircle } from "react-icons/ai";
 
 export default function Home() {
   return (
-    <div className="font-serif">
+    <div>
       {/*top*/}
-      <Image
-        src={"/ahaha.jpg"}
-        width={5000}
-        height={5000}
-        className="w-full h-full z-[-8] absolute top-0 object-cover brightness-[70%]"
-      ></Image>
-      <div className="h-screen w-full top-0 absolute z-[-1] gradient"></div>
+
       {/*top-nav*/}
-      <div
-        dir="rtl"
-        className="flex justify-between items-center mx-2 sm:mx-5 lg:mx-20 mt-4 text-[16px] sm:text-[18px] lg:text-[21px] text-white"
-      >
-        <p className="text-[24px] sm:text-[29px] lg:text-[35px] font-bold">
-          اسكولا
-        </p>
-        <div className="flex">
-          <p className="ml-5 sm:ml-8"> الصفحة الرئيسية</p>
-          <p className="ml-5 sm:ml-8"> الصفحة الصفحه</p>
-          <p> انضم لنا</p>
-        </div>
-      </div>
-      {/*top-text*/}
-      <div className="flex justify-center">
-        <p className="font-bold text-[25px] md:text-[30px] lg:text-[40px] text-center mb-4 mt-[28%] sm:mt-[18%] md:mt-[15%] lg:mt-[14%] text-white">
-          ما هو اسكولا ؟
-        </p>
-      </div>
-      <div className="flex justify-center">
-        <p
+      <div className="w-full h-screen gradient">
+        <Image
+          src={"/ahaha.jpg"}
+          width={5000}
+          height={5000}
+          className="w-full h-full z-[-8] absolute object-cover brightness-[70%]"
+        ></Image>
+
+        <div
           dir="rtl"
-          className="text-[19px] md:text-[23px] text-center w-[80%] md:w-[60%] lg:w-[50%] text-white"
+          className="flex justify-between items-center mx-2 sm:mx-5 lg:mx-20 text-[16px] sm:text-[18px] lg:text-[21px] text-white"
         >
-          تواجه بعض المدارس الكبيرة عدة مشاكل من الناحية التنظيمية والتي نهدف
-          الي حلها هنا في اسكولا بأقل تكلفة وسهولة تامة.
-        </p>
-      </div>
-      <div className="flex justify-center text-[40px] md:text-[50px] mt-[5rem] lg:mt-[3rem] xl:mt-[8rem] text-white">
-        <AiOutlineDownCircle></AiOutlineDownCircle>
+          <p className="text-[24px] sm:text-[29px] lg:text-[35px] font-bold mt-4">
+            اسكولا
+          </p>
+          <div className="mt-4 hidden sm:flex">
+            <p className="ml-5 sm:ml-8"> الصفحة الرئيسية</p>
+            <p className="ml-5 sm:ml-8"> الصفحة الصفحه</p>
+            <p> انضم لنا</p>
+          </div>
+        </div>
+        {/*top-text*/}
+        <div className="flex flex-col justify-center items-center h-full mt-[-3rem]">
+          <p className="font-bold text-[25px] md:text-[30px] lg:text-[40px] mb-4 text-white">
+            ما هو اسكولا ؟
+          </p>
+          <p
+            dir="rtl"
+            className="text-[19px] md:text-[23px] text-center w-[80%] md:w-[60%] lg:w-[50%] text-white"
+          >
+            تواجه بعض المدارس الكبيرة عدة مشاكل من الناحية التنظيمية والتي نهدف
+            الي حلها هنا في اسكولا بأقل تكلفة وسهولة تامة.
+          </p>
+        </div>
+        <div className="flex justify-center text-[40px] md:text-[50px] text-white mt-[-8rem]">
+          <AiOutlineDownCircle></AiOutlineDownCircle>
+        </div>
       </div>
       {/*end of top*/}
       {/*--------------------------------------------------------*/}
