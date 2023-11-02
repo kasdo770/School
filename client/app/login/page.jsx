8960ec9dae51ac494836ab4aa67c3e00deaf051b
Jeyenor <78587875+Jeyenore26@users.noreply.github.com>
2023-11-02 22:32:16 +0200

    312312

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -4,72 +4,82 @@ import React, { useState } from 'react';
 import Link from 'next/link'
 import { CgProfile } from "react-icons/cg";
 import { FaHashtag } from 'react-icons/fa'
-import { AiOutlineLock } from 'react-icons/ai'
+import { AiOutlineSearch } from 'react-icons/ai'
 export default function Home() {
   const [code, setcode] = useState("");
-  const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
   const [passerror, setpasserror] = useState("");
 
   function Validate() {
-    if (password && code && email) {
-      //send data
+    if (password && code) {
+      setpasserror("")
     }
     else {
       setpasserror("واحدة من الخانات فارغة")
     }
   }
   return (
-    <div className="flex lg:flex-row justify-center lg:justify-between  h-screen overflow-hidden">
-      <Image src={"/right5.png"} width={4000}
-        height={5000}
-        className="lg:w-[50%] lg:rotate-0 select-none  -rotate-90 lg:relative z-[-1] absolute m-0 p-0 hidden lg:block lg:h-screen " />
-      <Image src={"/mobilesignin.png"} width={4000}
-        height={5000}
-        className="lg:w-[50%] lg:rotate-0  lg:relative z-[-1] absolute m-0 p-0 w-[300%] block lg:hidden bottom-0 h-[16rem] lg:h-screen " />
-      <container className="h-max w-max mx-auto  md:mx-[6rem] xl:mx-[12rem] bg-transparent">
+    <>
+      <div className="flex lg:flex-row justify-center lg:justify-end relative items-center  h-screen overflow-hidden w-full">
 
-        <div dir="rtl" className="flex justify-center lg:justify-start items-center gap-4 mt-10 ">
-          <Image src={"/logo.png"} width={30} height={30} />
-          <h1 className=" text-2xl font-bold">اسكولا</h1>
+        <Image src={"/right2.svg"} width={5000}
+          height={5000}
+          className=" select-none h-full w-auto left-0  z-[-1] absolute hidden lg:block " />
 
-        </div>
 
-        <div dir="rtl" className="flex justify-start flex-col items-end  mt-[40%]  ">
-          <h1 className="flex justify-start text-4xl font-black mb-10">اهلا بك الى اسكولا</h1>
-          <section className='flex mt-5 mb-1 w-full z-[2] text-[20px] p-[5px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
-            <CgProfile className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
+        <Image src={"/mobilesignin.png"} width={4000}
+          height={5000}
+          className="lg:w-[50%] lg:rotate-0  lg:relative z-[-1] absolute m-0 p-0 w-[300%] block lg:hidden bottom-0 h-[16rem] lg:h-screen " />
+        <container className=" w-[20rem] mx-auto  md:mx-[6rem] xl:mx-[12rem] bg-transparent">
 
-            <input placeholder="أسم المدرسة" className="px-2 text-sm w-full outline-none border-0 bg-transparent ">
-            </input>
-          </section>
-          <section className='flex mt-5 mb-1 w-full z-[2] text-[20px] p-[5px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
-            <FaHashtag className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
+          <div dir="rtl" className="flex flex-col justify-center lg:justify-start items-center gap-4 my-4">
+            <Link href='/' className="hover:cursor-pointer">
 
-            <input placeholder="الرقم القومي" className="px-2 w-full text-sm outline-none border-0 bg-transparent ">
-            </input>
-          </section>
-          <section className='flex group mt-5 mb-1 w-full z-[2] text-[20px] p-[5px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
-            <AiOutlineLock className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
-
-            <input placeholder="كلمة المرور" className="px-2 w-full text-sm outline-none border-0 bg-transparent focus:border-dotted ">
-            </input>
-          </section>
-          <section className="flex justify-center">
-            <Link href='/register' className="flex justify-start text-[#757982] border-b-[1px] border-[#686b74] p-1 hover:text-[#9196a1] hover:cursor-pointer ">
-              <text>لديك حساب؟</text>
+              <Image src={"/logo.png"} width={50} height={50} />
             </Link>
-          </section>
-          <button onClick={Validate} className="w-full rounded-lg border-[1px] z-[2] bg-[#FE7900] p-2 mb-4 text-white hover:bg-[#FD8D00] active:bg-[#FEAA00] hover:text-white text-extrabold mt-5 text-lg border-none">تاكيد</button>
+
+          </div>
+
+          <div dir="rtl" className="flex justify-start flex-col  ">
+            <h1 className="flex justify-start text-4xl font-black mb-10">اهلا بك الى اكسولا </h1>
+            <section className='flex mt-5 mb-1 w-full z-[2] text-[20px] p-[7px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
+              <CgProfile className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
+
+              <input value={code} onChange={(e) => { setcode(e.target.value) }} placeholder="الرقم القومي" className="px-2 text-sm w-full outline-none border-0 bg-transparent ">
+              </input>
+            </section>
+            <section className='flex mt-5 mb-1 w-full z-[2] text-[20px] p-[7px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
+              <FaHashtag className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
+
+              <input value={password} onChange={(e) => { setpassword(e.target.value) }} placeholder="كلمة المرور" className="px-2 w-full text-sm outline-none border-0 bg-transparent ">
+              </input>
+            </section>
+            {passerror && (
+              <label
+                className={`font-semibold text-sm text-red-600 ml-4`}
+              >
+                {passerror}
+              </label>
+            )}
+            <section className="flex justify-center">
+              <Link href='/register' className="flex justify-start text-[#757982] border-b-[1px] border-[#686b74] p-1 hover:text-[#9196a1] hover:cursor-pointer ">
+                <text>انشاء مدرسة</text>
+              </Link>
+            </section>
+            <button onClick={Validate} className="w-full rounded-lg border-[1px] z-[2] bg-[#FE7900] p-2 mb-4 text-white hover:bg-[#FD8D00] active:bg-[#FEAA00] hover:text-white text-extrabold mt-5 text-lg border-none">تاكيد</button>
 
 
 
 
-        </div>
+          </div>
 
 
 
-      </container>
-    </div>
+        </container>
+      </div>
+      <button onClick={Validate} className="absolute right-10 bottom-10 bg-[#FE7900] p-3 rounded-full hover:bg-[#FD8D00] active:bg-[#FEAA00]  ">
+        <AiOutlineSearch className="text-white w-5 h-5" />
+      </button>
+    </>
   )
 }
--- client/app/register/page.jsx
+++ client/app/register/page.jsx
@@ -3,74 +3,101 @@ import Image from "next/image";
 import React, { useState } from 'react';
 import { CgProfile } from "react-icons/cg";
 import { FaHashtag } from 'react-icons/fa'
-import { AiOutlineLock } from 'react-icons/ai'
+import { AiOutlineSearch } from 'react-icons/ai'
 import Link from "next/link";
 export default function Home() {
   const [name, setname] = useState("");
-  const [email, setemail] = useState("");
-  const [emerror, setemerror] = useState("");
+  const [code, setcode] = useState("");
+  const [codeerror, setcodeerror] = useState("");
   const [password, setpassword] = useState("");
   const [confirmpass, setconfirmpass] = useState("");
   const [passerror, setpasserror] = useState("");
   function Validate() {
 
-    if (password != "" && confirmpass != "" && name != "" && email != "") {
+    if (password != "" && confirmpass != "" && name != "" && code != "") {
       if (password != confirmpass) {
         setpasserror("كلنة المرور ليست متطابقة")
       }
       else {
         setpasserror("")
       }
-      if (!email.includes("@") || !email.includes(".")) {
-        setemerror("هذا الايميل خطا")
+      if (code.length !== 14) {
+        setcodeerror("هذه الرقم يجب اي يكون 14 رقم")
       }
       else {
-        setemerror("")
+        setcodeerror("")
       }
     }
     else {
       setpasserror("واحدة من الخانات فارغة")
     }
-    if (passerror == "" && emerror == "") {
+    if (passerror == "" && codeerror == "") {
       //send data
     }
   }
   return (
-    <div className="flex lg:flex-row justify-center lg:justify-between  h-screen overflow-hidden">
-      <Image src={"/right4.png"} width={4000}
-        height={5000}
-        className="lg:w-[70%] lg:rotate-0 select-none -rotate-90 lg:relative z-[-1] absolute m-0 p-0 hidden lg:block lg:h-screen " />
-      <Image src={"/mobilesignin.png"} width={4000}
+    <div dir="rtl" className="flex lg:flex-row justify-center lg:justify-end relative items-center  h-screen overflow-hidden w-full">
+      <svg width="566" height="552" viewBox="0 0 566 552" fill="none" filter="hue-rotate(270deg)" xmlns="http://www.w3.org/2000/svg" className=" select-none h-full w-auto right-0  z-[-1] absolute hidden lg:block ">
+        <path d="M64 72.1962L175 0H563V552H64L22.5 498.856V420.643L0 356.469L97.5 311.847L86 281.264L188.6 235.139L190 214.082L6.5 140.381L64 72.1962Z" fill="#B673F9" />
+        <path d="M251 69L172.5 0H566V552H130L91.5 496.5L213.5 427L113.5 350.5L86 282L228.5 212L91.5 138.5L251 69Z" fill="#A858F7" />
+        <path d="M228 69.7468L245.5 0H566V551H265V496.199L185 424.459L277.5 352.222L319 282.475L215 211.731L319 145.472L228 69.7468Z" fill="#983AF4" />
+        <path d="M300.667 73.2346L264.053 0H566V552H355.839L247 493.213L275.087 426.455L287.627 355.213L355.839 283.473L300.667 211.235L275.087 137.004L300.667 73.2346Z" fill="#8823E5" />
+        <path d="M422.5 71.8049L352.5 0H566V552H403L337 494.157L438 418.862L370.5 277.247L358.5 209.93L370.5 145.106L422.5 71.8049Z" fill="#791ECC" />
+        <path d="M414 74.2691V0H566V552H468L451 501.316L432 425.542L496.5 357.796L443.5 283.025L496.5 215.782L486.5 138.502L414 74.2691Z" fill="#6A19B3" />
+      </svg>
+      <Image src={"/bluewave.png"} width={4000}
         height={5000}
         className="lg:w-[50%] lg:rotate-0  lg:relative z-[-1] absolute m-0 p-0 w-[300%] block lg:hidden bottom-0 h-[16rem] lg:h-screen " />
-      <container className="h-max w-max mx-auto  md:mx-[6rem] xl:mx-[12rem] bg-transparent">
+      <container className=" w-[20rem] mx-auto  md:mx-[6rem] xl:mx-[12rem] bg-transparent">
 
-        <div dir="rtl" className="flex justify-center lg:justify-start items-center gap-4 mt-10 ">
-          <Image src={"/logo.png"} width={60} height={60} />
-          <h1 className=" text-2xl font-bold">اسكولا</h1>
+        <div dir="rtl" className="flex flex-col justify-center lg:justify-start items-center gap-4 my-4">
+          <Link href='/' className="hover:cursor-pointer">
+
+            <Image src={"/logo.png"} width={50} height={50} />
+          </Link>
 
         </div>
 
-        <div dir="rtl" className="flex justify-start flex-col items-end  mt-[60%]  ">
-          <h1 className="flex justify-start text-4xl font-black mb-10">اهلا بك الى اسكولا</h1>
-          <section className='flex mt-5 mb-1 w-full z-[2] text-[20px] p-[5px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
+        <div dir="rtl" className="flex justify-start flex-col items-center ">
+          <h1 className="flex justify-start text-4xl font-black mb-10">اهلا بك الى اكسولا </h1>
+          <section className='flex mt-5 mb-1 w-full z-[2] text-[20px] p-[7px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
             <CgProfile className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
 
-            <input placeholder="أسم المدرسة" className="px-2 text-sm w-full outline-none border-0 bg-transparent ">
+            <input value={name} onChange={(e) => { setname(e.target.value) }} placeholder="اسم المدرسة" className="px-2 text-sm w-full outline-none border-0 bg-transparent ">
             </input>
           </section>
-          <section className='flex mt-5 mb-1 w-full z-[2] text-[20px] p-[5px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
+          <section className='flex mt-5 mb-1 w-full z-[2] text-[20px] p-[7px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
             <FaHashtag className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
 
-            <input placeholder="الرقم القومي" className="px-2 w-full text-sm outline-none border-0 bg-transparent ">
+            <input value={code} onChange={(e) => { setcode(e.target.value) }} placeholder="الرقم القومي" className="px-2 w-full text-sm outline-none border-0 bg-transparent ">
             </input>
           </section>
-          <section className='flex group mt-5 mb-1 w-full z-[2] text-[20px] p-[5px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
-            <AiOutlineLock className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
+          {codeerror && (
+            <label
+              className={`font-semibold text-sm text-red-600 ml-4`}
+            >
+              {codeerror}
+            </label>
+          )}
+          <section className='flex group mt-5 mb-1 w-full z-[2] text-[20px] p-[7px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
+            <AiOutlineSearch className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
 
-            <input placeholder="كلمة المرور" className="px-2 w-full text-sm outline-none border-0 bg-transparent focus:border-dotted ">
+            <input value={password} onChange={(e) => { setpassword(e.target.value) }} placeholder="كلمة المرور" className="px-2 w-full text-sm outline-none border-0 bg-transparent focus:border-dotted ">
             </input>
           </section>
+          <section className='flex group mt-5 mb-1 w-full z-[2] text-[20px] p-[7px] rounded-md outline-none border-solid items-center border-black  border-[1px]'>
+            <AiOutlineSearch className="w-6 h-6 pl-1 border-black border-l-[1px] ml-1" />
+
+            <input value={confirmpass} onChange={(e) => { setconfirmpass(e.target.value) }} placeholder="تاكيد كلمة المرور" className="px-2 w-full text-sm outline-none border-0 bg-transparent focus:border-dotted ">
+            </input>
+          </section>
+          {passerror && (
+            <label
+              className={`font-semibold text-sm text-red-600 ml-4`}
+            >
+              {passerror}
+            </label>
+          )}
           <section className="flex justify-center">
             <Link href='/login' className="flex justify-start text-[#757982] border-b-[1px] border-[#686b74] p-1 hover:text-[#9196a1] hover:cursor-pointer ">
               <text>لديك حساب؟</text>
