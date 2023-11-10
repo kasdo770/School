ae6aaf19209f35311361f8a0761ceab4ae7d481f
Jeyenor <78587875+Jeyenore26@users.noreply.github.com>
2023-11-10 17:28:21 +0200

    312

--- client/app/parent/page.jsx
+++ client/app/parent/page.jsx
@@ -6,20 +6,19 @@ import { CgProfile } from 'react-icons/cg'
 import { Students } from '../components/dashboard-comps/Students'
 import Skeleton from '../components/Skeleton'
 export default function page() {
-    const [page, setPage] = useState(1);
-    const [loading, setloading] = useState(true)
-    const data = ["", "", ""]
+    const [page, setPage] = useState(2);
+    const [loading, setloading] = useState(false)
+    const absent = 4
+    const data = [""]
     return (
         <>
             <div className="flex flex-col w-full text-white bg-white">
                 <div className="bg-[#009DD8] border-r-[1px] flex justify-between items-center p-3">
-                    <button className="flex items-center gap-1 hover:text-[#ff7000] active:text-[#ff6f00da]">
-                        <h1 className="sm:ml-5 text-[15px] md:text-[18px]">
-                            بياناتك
-                        </h1>
-                        <CgProfile className='w-7 mx-1 mt-1 h-7' />
-
-                    </button>
+                    <a
+                        href="/"
+                        className="ml-5 sm:ml-8 bg-[#C16514] p-2 rounded-lg hover:opacity-[60%] transition-sm"
+                    >
+                        العودة                     </a>
 
                     <div className="flex items-center transition-sm gap-x-4 ">
                         <button className="flex items-center gap-1 hover:text-[#d3d3d3] w-20 h-fit  " onClick={(() => {
@@ -31,12 +30,12 @@ export default function page() {
                             </h1>
 
                         </button>
-                        <button className="flex items-center gap-1 hover:text-[#d3d3d3] w-20 h-fit  " onClick={(() => {
+                        <button className="flex items-center gap-1 hover:text-[#d3d3d3] w-[7rem] h-fit  " onClick={(() => {
                             setPage(2)
                         })}>
-                            <h1 className={`sm:ml-5 text-[15px] md:text-[18px] border-b-[4px] w-20  transition-all duration-500  ${page == 2 ? "border-b-[#ff7000] 	 w-20" : "border-b-white border-b-0 w-0 	"
+                            <h1 className={`sm:ml-5 text-[15px] md:text-[18px] border-b-[4px] w-[7rem]  transition-all duration-500  ${page == 2 ? "border-b-[#ff7000] 	 w-20" : "border-b-white border-b-0 w-0 	"
                                 }`}>
-                                بياناتك
+                                حضور ابنك
                             </h1>
 
                         </button>
@@ -68,8 +67,25 @@ export default function page() {
                         ))}
                     </div>
                 </div>
-                <div className={`${page == 2 ? "block" : "hidden"}`}>
-                    <Students />
+                <div className={` ${page == 2 ? "block" : "hidden"}`}>
+                    <Skeleton loading={loading} numbers={data} classname={"md:w-[25rem] md:h-[15rem] w-[13rem] h-[12rem]"} ></Skeleton>
+
+                    <div className={`border  border-blue-300 shadow rounded-md p-10  md:w-[25rem] md:h-[15rem] w-[13rem] h-[12rem]  mx-auto ${loading == false ? "block" : "hidden"}`} >
+
+                        <div dir='rtl' className='flex justify-between'>
+                            <h1 className='mt-4'>اسم الطالب</h1>
+                            <h1 className='mt-4' >عمار خرا</h1>
+                        </div>
+                        <div dir='rtl' className='flex justify-between'>
+                            <h1 className='mt-4'>الرقم القومي</h1>
+                            <h1 className='mt-4' >69696969696969</h1>
+                        </div>
+                        <div dir='rtl' className='flex justify-between'>
+                            <h1 className='mt-4'>عدد خضوره</h1>
+                            <h1 className={`mt-4 ${absent > 3 ? "text-red-700 font-bold" : "text-black"}`} >5</h1>
+                        </div>
+
+                    </div>
                 </div>
             </div >
         </>
