7f35211c1ac08e6e7112fc205258bb1a71a23f43
Jeyenor <78587875+Jeyenore26@users.noreply.github.com>
2023-11-10 21:28:18 +0200

    321

--- client/app/teacher/page.jsx
+++ client/app/teacher/page.jsx
@@ -21,19 +21,11 @@ export default function page() {
                         العودة                     </a>
 
                     <div className="flex items-center transition-sm gap-x-4 ">
+
                         <button className="flex items-center gap-1 hover:text-[#d3d3d3] w-[7rem] h-fit  " onClick={(() => {
                             setPage(1)
                         })}>
-                            <h1 className={`sm:ml-5 text-[15px] md:text-[18px] border-b-[4px] w-[7rem] transition-all duration-500   ${page == 1 ? "border-b-[#ff7000] 	 w-20" : "border-b-white border-b-0 w-0 	"
-                                }`}>
-                                ارسال تحذير
-                            </h1>
-
-                        </button>
-                        <button className="flex items-center gap-1 hover:text-[#d3d3d3] w-[7rem] h-fit  " onClick={(() => {
-                            setPage(2)
-                        })}>
-                            <h1 className={`sm:ml-5 text-[15px] md:text-[18px] border-b-[4px] w-[7rem]  transition-all duration-500  ${page == 2 ? "border-b-[#ff7000] 	 w-20" : "border-b-white border-b-0 w-0 	"
+                            <h1 className={`sm:ml-5 text-[15px] md:text-[18px] border-b-[4px] w-[7rem]  transition-all duration-500 pb-2 ${page == 1 ? "border-b-[#ff7000] 	 w-20" : "border-b-white border-b-0 w-0 	"
                                 }`}>
                                 بياناتك
                             </h1>
@@ -42,7 +34,7 @@ export default function page() {
                         <button className="flex items-center gap-1 hover:text-[#d3d3d3] w-[7rem] h-fit  " onClick={(() => {
                             setPage(2)
                         })}>
-                            <h1 className={`sm:ml-5 text-[15px] md:text-[18px] border-b-[4px] w-[7rem]  transition-all duration-500  ${page == 2 ? "border-b-[#ff7000] 	 w-20" : "border-b-white border-b-0 w-0 	"
+                            <h1 className={`sm:ml-5 text-[15px] md:text-[18px] border-b-[4px] w-[7rem]  transition-all duration-500 pb-2  ${page == 2 ? "border-b-[#ff7000] 	 w-20" : "border-b-white border-b-0 w-0 	"
                                 }`}>
                                 الحضور
                             </h1>
@@ -50,15 +42,14 @@ export default function page() {
                         </button>
 
                     </div>
-                    <div className="flex items-center transition-sm   rounded-sm ">
+                    <div className="flex items-center transition-sm   rounded-sm gap-x-5 ">
                         <h1 className="sm:ml-5 text-[15px] md:text-[21px]">
 
                             اسكولا
                         </h1>
-                        <Link href='/' className="hover:cursor-pointer ml-3">
+                        <Image src={"/logo.png"} width={50} height={50} />
 
-                            <Image src={"/logo.png"} width={50} height={50} />
-                        </Link>
+
                     </div>
                 </div>
             </div>
@@ -79,19 +70,19 @@ export default function page() {
                 <div className={` ${page == 2 ? "block" : "hidden"}`}>
                     <Skeleton loading={loading} numbers={data} classname={"md:w-[25rem] md:h-[15rem] w-[13rem] h-[12rem]"} ></Skeleton>
 
-                    <div className={`border  border-blue-300 shadow rounded-md p-10  md:w-[25rem] md:h-[15rem] w-[13rem] h-[12rem]  mx-auto ${loading == false ? "block" : "hidden"}`} >
+                    <div className={`border flex items-center flex-col justify-center gap-y-10  border-blue-300 shadow rounded-md   md:w-[25rem] md:h-[15rem] w-full h-[12rem]  mx-auto ${loading == false ? "block" : "hidden"}`} >
 
                         <div dir='rtl' className='flex justify-between'>
-                            <h1 className='mt-4'>اسم الطالب</h1>
-                            <h1 className='mt-4' >عمار خرا</h1>
+                            <h1 className='mx-[1rem]mx-5'>اسم الطالب</h1>
+                            <h1 className='mx-[1rem]' >عمار خرا</h1>
                         </div>
                         <div dir='rtl' className='flex justify-between'>
-                            <h1 className='mt-4'>الرقم القومي</h1>
-                            <h1 className='mt-4' >69696969696969</h1>
+                            <h1 className='mx-[1rem]'>الرقم القومي</h1>
+                            <h1 className='mx-[1rem]' >69696969696969</h1>
                         </div>
                         <div dir='rtl' className='flex justify-between'>
-                            <h1 className='mt-4'>عدد خضوره</h1>
-                            <h1 className={`mt-4 ${absent > 3 ? "text-red-700 font-bold" : "text-black"}`} >5</h1>
+                            <h1 className='mx-[1rem]'>عدد خضوره</h1>
+                            <h1 className={` ${absent > 3 ? "text-red-700 font-bold" : "text-black"}`} >5</h1>
                         </div>
 
                     </div>
